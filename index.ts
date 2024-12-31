fcce61c7f7ab15323623859b986c5a1adf967782
Evaan Chowdhry <100704426+Phantom8015@users.noreply.github.com>
2024-12-31 10:47:46 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const client = new DiscordJS.Client({
 })
 
 client.on('ready', () => {
-
+    console.log("Goon is ready")
     new WOKCommands(client, {
         commandsDir: path.join(__dirname, 'commands'),
         
